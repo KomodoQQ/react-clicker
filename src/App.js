13c6be832efb0bbfe8aad3395cc15512936fbd52
Mykhailo Kovalenko <Mykhailo_Kovalenko@epam.com>
2021-02-10 12:39:00 +0200

    first push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Game } from "./components/game/component"
+
 function App() {
   return (
     <div className="App">
+      <Game></Game>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/game/component.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Game = () => {
+  return <div>
+    <h2>Game component</h2>
+  </div>
+}
